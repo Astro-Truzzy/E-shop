b662ac26f01b3799435a0d3a6bb47ffc2d52b421
Astro-Truzzy <taresy.dev@gmail.com>
2025-01-24 10:37:50 +0100

    finished navbar and hero section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Category from "./Components/Category";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/hero/Hero"
+
+const App = () => {
+  return (
+    <div className="">
+    <Navbar/>
+    <Hero/>
+    <Category/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Category.jsx
@@ -0,0 +1,13 @@
+
+
+const Category = () => {
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Category
